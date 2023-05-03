d4f9712446ebd9ee8948f47f1bbaeff3ab26db2c
Brandon Tilstra <brandontilstra@promocat.nl>
2023-05-03 15:28:43 +0200

    lazy construction of cdn service

--- src/ObjectStore/v1/Service.php
+++ src/ObjectStore/v1/Service.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PromoCat\Rackspace\ObjectStore\v1;
 
 use OpenStack\Common\Resource\ResourceInterface;
+use OpenStack\Common\Service\Builder;
 use PromoCat\Rackspace\ObjectStore\v1\CDN\Service as CdnService;
 use PromoCat\Rackspace\ObjectStore\v1\Models\Container;
 use PromoCat\Rackspace\ObjectStore\v1\Models\HasInitializedService;
@@ -16,6 +17,9 @@ use PromoCat\Rackspace\ObjectStore\v1\Models\HasInitializedService;
  */
 class Service extends \OpenStack\ObjectStore\v1\Service
 {
+    private Builder $builder;
+    private array $cdnOptions = [];
+
     private CdnService $cdnService;
 
     public function model(string $class, $data = null): ResourceInterface
@@ -34,6 +38,13 @@ class Service extends \OpenStack\ObjectStore\v1\Service
 
     public function getCdnService(): CdnService
     {
+        if (!isset($this->cdnService)) {
+            if (!isset($this->builder)) {
+                throw new \Exception('The service builder should be set to create the requested CDN Service.');
+            }
+            $this->cdnService = $this->builder->createService('ObjectStore\\v1\\CDN', $this->cdnOptions);
+        }
+
         return $this->cdnService;
     }
 
@@ -42,5 +53,18 @@ class Service extends \OpenStack\ObjectStore\v1\Service
         $this->cdnService = $cdnService;
 
         return $this;
+    }
+
+    public function setServiceBuilder(Builder $builder)
+    {
+        $this->builder = $builder;
+    }
+
+    public function setCdnOptions(array $cdnOptions = [])
+    {
+        $this->cdnOptions = array_merge([
+            'catalogName' => 'cloudFilesCDN',
+            'catalogType' => 'rax:object-cdn',
+        ], $cdnOptions);
     }
 }
--- src/Rackspace.php
+++ src/Rackspace.php
@@ -12,7 +12,7 @@ use OpenStack\Common\Transport\HandlerStack;
 use OpenStack\Common\Transport\Utils;
 use OpenStack\OpenStack;
 use PromoCat\Rackspace\Identity\Service as IdentityService;
-use PromoCat\Rackspace\ObjectStore\v1\CDN\Service as CDNService;
+use PromoCat\Rackspace\ObjectStore\v1\CDN\Service as CdnService;
 use PromoCat\Rackspace\ObjectStore\v1\Service;
 
 class Rackspace extends OpenStack
@@ -72,18 +72,15 @@ class Rackspace extends OpenStack
      */
     public function objectStoreV1(array $options = [], array $cdnOptions = []): Service
     {
-        /** @var CDNService $cdnService */
-        $cdnService = $this->builder->createService('ObjectStore\\v1\\CDN', array_merge([
-            'catalogName' => 'cloudFilesCDN',
-            'catalogType' => 'rax:object-cdn',
-        ], $cdnOptions));
-
         /** @var Service $objectStoreService */
         $objectStoreService = $this->builder->createService('ObjectStore\\v1', array_merge([
             'catalogName' => 'cloudFiles',
             'catalogType' => 'object-store',
         ], $options));
 
-        return $objectStoreService->setCdnService($cdnService);
+        $objectStoreService->setServiceBuilder($this->builder);
+        $objectStoreService->setCdnOptions($cdnOptions);
+
+        return $objectStoreService;
     }
 }
